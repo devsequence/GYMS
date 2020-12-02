d3c61caf254f72813be982f776159b4931461ede
Slipak Bohdan <fear3494@gmail.com>
2020-12-02 08:38:08 +0200

    home page

--- static/js/common.js
+++ static/js/common.js
@@ -1,11 +1,66 @@
 AOS.init();
+$(window).on('scroll', function() {
+    var $this = $(this);
+
+    if ($this.scrollTop() > 1) {
+        $('.header').addClass('header-scroll');
+    }else {
+        $('.header').removeClass('header-scroll');
+    }
+
+});
+$('.counter-block__numb').each(function () {
+    var $this = $(this);
+        $this.prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+
+});
+$(".header-nav__list a[href^='#']").on('click', function (e){
+    e.preventDefault();
+    var $this = $(this);
+    $('html, body').animate({
+        scrollTop: $($this.attr("href")).offset().top - 100
+    }, 1200);
+    $('.header-nav, .header-btn').removeClass('is-open');
+});
+
 var windowWidth = $(window).width();
-if (windowWidth < 992) {
+if (windowWidth < 768) {
     // mobileScrollAnimate();
 
+    $('.service-item').on('click', function (e) {
+        e.preventDefault();
+        var $this = $(this);
+        var $thisList = $this.find('.service-item__list');
+        if($this.hasClass('is-open')){
+            $('.service-item').removeClass('is-open');
+            $this.removeClass('is-open');
+        }else{
+            $('.service-item').removeClass('is-open');
+            $this.addClass('is-open');
+        }
+    });
 }
 
 else {
+    $('.service-item').on('click', function (e) {
+        e.preventDefault();
+        var $this = $(this);
+        var $thisTitle = $this.find('.service-item__title');
+        var $thisList = $this.find('.service-item__list');
+        var $thisImage = $this.find('.service-item__media');
+        $('.service-info__title').html($thisTitle.html());
+        // $('.service-media img').attr('src');
+        $('.service-info__list').html($thisList.html());
+    });
+
     $('.main-wrapper').on('mousemove',function(e){
         var paralax = $('.parallax img');
         var $pageY = 50+e.pageY/20;
@@ -48,7 +103,12 @@ $('.dropdown-title').each(function () {
     });
 });
 
-
+$('.header-btn').on('click', function (e) {
+    e.preventDefault();
+    var $this = $(this);
+    $this.toggleClass('is-open');
+    $('.header-nav').toggleClass('is-open');
+});
 $('.sidebar-slider').slick({
     prevArrow:"<button type='button' class='slick-prev pull-left'><svg class=\"icon\"><use xlink:href=\"#arrow\"></use></svg></button>",
     nextArrow:"<button type='button' class='slick-next pull-right'><svg class=\"icon\"><use xlink:href=\"#arrow\"></use></svg></button>",
@@ -56,6 +116,29 @@ $('.sidebar-slider').slick({
 $('.main-slider').slick({
     prevArrow:"<button type='button' class='slick-prev pull-left'><svg class=\"icon\"><use xlink:href=\"#arrow\"></use></svg></button>",
     nextArrow:"<button type='button' class='slick-next pull-right'><svg class=\"icon\"><use xlink:href=\"#arrow\"></use></svg></button>",
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                arrows: false,
+                dots: true,
+                autoplay: true,
+                autoplaySpeed: 2000,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                speed: 500,
+                fade: true,
+                cssEase: 'linear',
+                arrows: false,
+                dots: true,
+                autoplay: true,
+                autoplaySpeed: 2000,
+            }
+        }
+    ]
 });
 $('.news-slider').slick({
     slidesToShow: 4,
@@ -70,7 +153,7 @@ $('.news-slider').slick({
             }
         },
         {
-            breakpoint: 992,
+            breakpoint: 768,
             settings: {
                 slidesToShow: 2,
             }
